86fa967593986b34b35fc7e931e59b772e9bb6d7
hsy <991279557@qq.com>
2025-02-06 23:11:19 +0800

    项目初始化

--- src/main/java/com/yupi/yupicturebackend/YuPictureBackendApplication.java
+++ src/main/java/com/yupi/yupicturebackend/YuPictureBackendApplication.java
@@ -1,9 +1,11 @@
 package com.yupi.yupicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yupi.yupicturebackend.mapper")
 public class YuPictureBackendApplication {
 
     public static void main(String[] args) {
