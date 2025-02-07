8ca7214a0bb68634fbd2d9ca3bfd48352f1d5ca9
hsy <991279557@qq.com>
2025-02-07 19:27:31 +0800

    用户模块

---
+++ src/main/java/com/yupi/yupicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yupicturebackend.mapper;
+
+import com.yupi.yupicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 黄思源
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-02-07 13:17:59
+* @Entity com.yupi.yupicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
