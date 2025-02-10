0d1310119cc9e293c0595b6b48ca64bcb206b181
hsy <991279557@qq.com>
2025-02-10 16:21:21 +0800

    图片模块0

--- src/main/java/com/yupi/yupicturebackend/manager/CosManager.java
+++ src/main/java/com/yupi/yupicturebackend/manager/CosManager.java
@@ -1,10 +1,13 @@
 package com.yupi.yupicturebackend.manager;
 
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.PutObjectResult;
 import com.yupi.yupicturebackend.config.CosClientConfig;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.File;
 
 @Component
 public class CosManager {  
@@ -15,5 +18,17 @@ public class CosManager {
     @Resource  
     private COSClient cosClient;
   
-    // ... 一些操作 COS 的方法  
+    // ... 一些操作 COS 的方法
+    /**
+     * 上传对象
+     *
+     * @param key  唯一键
+     * @param file 文件
+     */
+    public PutObjectResult putObject(String key, File file) {
+        PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key,
+                file);
+        return cosClient.putObject(putObjectRequest);
+    }
+
 }
